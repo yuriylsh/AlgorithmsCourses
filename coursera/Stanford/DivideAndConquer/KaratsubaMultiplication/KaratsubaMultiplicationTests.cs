9c86e23e96604fd39974b1013ee487700f95bf04
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-06-09 13:59:22 -0500

    Removing FluentAssertions as there are not enough tests to warrant its usage.

--- coursera/Stanford/DivideAndConquer/KaratsubaMultiplication/KaratsubaMultiplicationTests.cs
+++ coursera/Stanford/DivideAndConquer/KaratsubaMultiplication/KaratsubaMultiplicationTests.cs
@@ -1,5 +1,4 @@
 using System;
-using FluentAssertions;
 using Xunit;
 using static KaratsubaMultiplication.Utils;
 
@@ -16,7 +15,7 @@ namespace KaratsubaMultiplication
         [InlineData("66729899957367990709339108031112", "32340867005124202389268050080611", "2158102819786481370385134761940486771461182643704842409095969432")]
         [InlineData("3141592653589793238462643383279502884197169399375105820974944592", "2718281828459045235360287471352662497757247093699959574966967627", "8539734222673567065463550869546574495034888535765114961879601127067743044893204848617875072216249073013374895871952806582723184")]
         [InlineData("54912929456625402256174854408349005059747367418045281224869677103460587001371838509047325775339825409089700424382586661886548455", "46159761840763928340146255755371712831239294454920928659343103820160462080093196044837908676121279113445132081834876936121057294", "2534767745696498721291598226410808420875926624404989594109011804054230420328163716811636469827804550673802278410568911221048870743264706668346786117132264407551450821426489441139772838020127634525234251586349419324977858145598895358201970734412370962180770")]
-        public void MultiplyTests(string first, string second, string expectedResult) => Multiply(first, second).Should().Be(expectedResult);
+        public void MultiplyTests(string first, string second, string expectedResult) => Assert.Equal(expectedResult, Multiply(first, second));
 
         public static string Multiply(ReadOnlySpan<char> first, ReadOnlySpan<char> second)
         {
@@ -45,7 +44,7 @@ namespace KaratsubaMultiplication
         [InlineData("1", "2", "3")]
         [InlineData("19", "1", "20")]
         [InlineData("999", "9", "1008")]
-        public void AddTests(string first, string second, string expectedResult) => Add(first, second).Should().Be(expectedResult);
+        public void AddTests(string first, string second, string expectedResult) => Assert.Equal(expectedResult, Add(first, second));
 
         public static string Add(ReadOnlySpan<char> first, ReadOnlySpan<char> second)
         {
@@ -71,7 +70,7 @@ namespace KaratsubaMultiplication
         [InlineData("10", "1", "9")]
         [InlineData("1008", "9", "999")]
         [InlineData("18", "8", "10")]
-        public void SubstractTest(string first, string second, string expectedResult) => Substract(first, second).Should().Be(expectedResult);
+        public void SubstractTest(string first, string second, string expectedResult) => Assert.Equal(expectedResult,  Substract(first, second));
 
         public static string Substract(ReadOnlySpan<char> first, ReadOnlySpan<char> second)
         {
@@ -95,6 +94,5 @@ namespace KaratsubaMultiplication
             
             return result.GetMathOperationResult().TrimLeadingZeroes();
         }
-        
     }
 }
