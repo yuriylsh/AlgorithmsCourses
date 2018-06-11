ea80b467b8c7df7aa8b5d543df2182bb1de131a7
Yuriy Lyeshchenko <yuriylsh@live.com>
2018-06-10 19:44:57 -0500

    Implementation of counting invervsion algorithm from https://www.coursera.org/learn/algorithms-divide-conquer/exam/YLbzP/programming-assignment-2

--- coursera/Stanford/DivideAndConquer/NumberOfInversions/InputLoadingTests.cs
+++ coursera/Stanford/DivideAndConquer/NumberOfInversions/InputLoadingTests.cs
@@ -11,12 +11,16 @@ namespace NumberOfInversions
     {
         public const string SampleInputFileName = "sampleinput.txt";
         public const string ProblemInputFileName = "probleminput.txt";
+        private static string _currentDirectoryPath;
 
         public static int[] LoadInput(string fileName) => 
-            File.ReadLines(GetInputPath(fileName))
+            File.ReadLines(Path.Combine(CurrentDirectoryPath, fileName))
                 .TakeWhile(line => line.Length > 0)
                 .Select(int.Parse)
                 .ToArray();
+
+        private static string CurrentDirectoryPath => _currentDirectoryPath 
+            ?? (_currentDirectoryPath = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path)));
 
         [Fact]
         private void LoadInput_Sample_ReturnsSampleInputAsArray()
@@ -25,18 +29,5 @@ namespace NumberOfInversions
         [Fact]
         private void LoadInput_Problem_ReturnsProblemInputAsArray()
             => LoadInput(ProblemInputFileName).Should().HaveCount(100_000);
-
-        private static string GetInputPath(string fileName) => Path.Combine(CurrentDirectoryPath, fileName);
-
-        private static string CurrentDirectoryPath => _currentDirectoryPath 
-            ?? (_currentDirectoryPath = Path.GetDirectoryName(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path)));
-
-        private static string _currentDirectoryPath; 
-
-        [Theory]
-        [InlineData(SampleInputFileName)]
-        [InlineData(ProblemInputFileName)]
-        private void InputFilesExistTests(string fileName)
-            => File.Exists(GetInputPath(fileName)).Should().BeTrue();
     }
 }
---
+++ coursera/Stanford/DivideAndConquer/NumberOfInversions/InverstionsCounterTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace NumberOfInversions
+{
+    public class InverstionsCounterTests
+    {
+        public static uint CountInversions(int[] input)
+            => SortAndCountInversions(input, 0, input.Length -1);
+
+        private static uint SortAndCountInversions(int[] input, int leftIndex, int rightIndex)
+        {
+            if (leftIndex == rightIndex) return 0;
+
+            var middle = (leftIndex + rightIndex) / 2;
+            var leftInversions = SortAndCountInversions(input, leftIndex, middle);
+            var rightInversions = SortAndCountInversions(input, middle + 1, rightIndex);
+            var splitInversions = MergeAndCountInversions(input, leftIndex, middle, rightIndex);
+            return leftInversions + rightInversions + splitInversions;
+        }
+
+        private static uint MergeAndCountInversions(Span<int> array, int leftIndex, int middleIndex, int rightIndex)
+        {
+            Span<int> leftArray = array.Slice(leftIndex, middleIndex - leftIndex + 1).ToArray();
+            Span<int> rightArray = array.Slice(middleIndex + 1, rightIndex - middleIndex).ToArray();
+
+            int leftCounter = 0, rightCounter = 0;
+            uint splitInversions = 0;
+            for (var i = leftIndex; i <= rightIndex; i++)
+            {
+                var copyRestSink = array.Slice(i + 1);
+                var isInversion = leftArray[leftCounter] > rightArray[rightCounter];
+                if (isInversion)
+                {
+                    splitInversions += (uint)(leftArray.Length - leftCounter);
+                    array[i] = rightArray[rightCounter++];
+                    if(CopyRestOfFirstIfSecondIsEmpty(leftArray.Slice(leftCounter), rightArray.Slice(rightCounter), copyRestSink)) break;
+                }
+                else
+                {
+                    array[i] = leftArray[leftCounter++];
+                    if(CopyRestOfFirstIfSecondIsEmpty(rightArray.Slice(rightCounter), leftArray.Slice(leftCounter), copyRestSink)) break;
+                }
+            }
+
+            return splitInversions;
+        }
+
+        private static bool CopyRestOfFirstIfSecondIsEmpty(Span<int> first, Span<int> second, Span<int> sink)
+        {
+            if (second.IsEmpty)
+            {
+                first.CopyTo(sink);
+            }
+            return second.IsEmpty;
+        }
+
+        [Theory]
+        [InlineData(InputLoading.SampleInputFileName, 5)]
+        [InlineData(InputLoading.ProblemInputFileName, 2407905288)]
+        private void Count_NonEmptyInput_ReturnsCorrectNumberOfInversions(string fileName, uint count) 
+            => CountInversions(InputLoading.LoadInput(fileName)).Should().Be(count);
+    }
+}
